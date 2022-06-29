a434dc4143427b6b600ada16f95d435d7bcb513e
Soneliem <24221762+Soneliem@users.noreply.github.com>
2022-06-29 18:39:00 +0930

    Add Inventory popup

--- WAIUA/Controls/InventoryControl.xaml.cs
+++ WAIUA/Controls/InventoryControl.xaml.cs
@@ -1,28 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WAIUA.Objects;
 
 namespace WAIUA.Controls
 {
-    /// <summary>
-    /// Interaction logic for InventoryControl.xaml
-    /// </summary>
     public partial class InventoryControl : UserControl
     {
-        public InventoryControl()
+        
+        public static readonly DependencyProperty SkinDataProperty =
+            DependencyProperty.Register("SkinDataObject", typeof(SkinData), typeof(InventoryControl), new PropertyMetadata(new SkinData()));
+
+        public InventoryControl(SkinData skinData)
         {
             InitializeComponent();
+            SkinDataObject = skinData;
         }
+
+        public SkinData SkinDataObject
+        {
+            get => (SkinData) GetValue(SkinDataProperty);
+            set => SetValue(SkinDataProperty, value);
+        }
+
+        public static readonly RoutedEvent CloseButtonEvent =
+            EventManager.RegisterRoutedEvent("SettingConfirmedEvent", RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler), typeof(InventoryControl));
+
+        public event RoutedEventHandler CloseButton
+            {
+                add { AddHandler(CloseButtonEvent, value); }
+                remove { RemoveHandler(CloseButtonEvent, value); }
+            }
+
+        private void CloseBtnClick(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(InventoryControl.CloseButtonEvent));
+
+        }
+
     }
 }
--- WAIUA/Controls/PlayerControl.xaml.cs
+++ WAIUA/Controls/PlayerControl.xaml.cs
@@ -2,21 +2,25 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Navigation;
 using WAIUA.Objects;
+using WAIUA.ViewModels;
+using WAIUA.Views;
 
 namespace WAIUA.Controls;
 
 public partial class PlayerControl : UserControl
 {
     public static readonly DependencyProperty PlayerProperty =
-        DependencyProperty.Register("PlayerCell", typeof(Player), typeof(PlayerControl), new PropertyMetadata(new Player()));
-
+        DependencyProperty.Register("PlayerCell", typeof(Player), typeof(PlayerControl), new PropertyMetadata(new Player()));    
     public PlayerControl()
     {
         InitializeComponent();
+        AddHandler(InventoryControl.CloseButtonEvent, 
+            new RoutedEventHandler(ClosePopupEventHandlerMethod));
     }
 
     public Player PlayerCell
@@ -35,6 +39,16 @@ public partial class PlayerControl : UserControl
 
     private void EventSetter_OnHandler(object sender, MouseButtonEventArgs e)
     {
-        
+        var s = sender as FrameworkElement;
+        var player = s.DataContext as Player;
+        popup.Child = new InventoryControl(player.SkinData);
+        popup.IsOpen = true;
+        e.Handled = true;
+    }
+
+    private void ClosePopupEventHandlerMethod(object sender,
+        RoutedEventArgs e)
+    {
+        popup.IsOpen = false;
     }
 }
